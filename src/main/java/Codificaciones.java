e096480b5c376fc8632823d668793103ccea4e87
Leandro <leandroperezb@gmail.com>
2019-05-16 14:27:51 -0300

    Huffman a nivel de bit

--- src/main/java/Codificaciones.java
+++ src/main/java/Codificaciones.java
@@ -1,21 +1,123 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.PriorityQueue;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.File;
+import java.nio.file.Files;
+import java.util.*;
 
 public class Codificaciones {
 
-    public static String codificarConHuffman(Imagen img){
-        StringBuilder salida = new StringBuilder();
-        Map<Integer, String> codificaciones = codificacionHuffman(img.probabilidadesSimples()).codificaciones;
+    public static char[] decodeSequence(String path) {
+        char[] restoredSequence = null;
+        try {
+            byte[] inputSequence = Files.readAllBytes(new File(path).toPath());
+            int globalIndex = 0;
+
+            //Obtener tamaño del encabezado
+            int tamanio = (((int) inputSequence[0]) << 24) | (((int) inputSequence[1]) << 16) | (((int) inputSequence[2]) << 8) | ((int) inputSequence[3]);
+
+
+            byte mask = (byte) (1 << (8 - 1)); // mask: 10000000
+            int bufferPos = 0;
+            int i = 4;
+            restoredSequence = new char[tamanio * 8];
+            while (globalIndex < tamanio*8)
+            {
+                byte buffer = inputSequence[i];
+                while (bufferPos < 8) {
+
+                    if ((buffer & mask) == mask) {
+                        restoredSequence[globalIndex] = '1';
+                    } else {
+                        restoredSequence[globalIndex] = '0';
+                    }
+
+                    buffer = (byte) (buffer << 1);
+                    bufferPos++;
+                    globalIndex++;
+
+                    if (globalIndex == tamanio*8) {
+                        break;
+                    }
+                }
+                i++;
+                bufferPos = 0;
+            }
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+
+        return restoredSequence;
+    }
+
+    private static byte[] convertBooleanListToByteArray(List<Boolean> input) {
+        List<Byte> lista = new ArrayList<>();
+        byte buffer = 0;
+        int bufferPos = 0;
+        int i = 0;
+        while (i < input.size()) {
+            buffer = (byte) (buffer << 1);
+            bufferPos++;
+            if (input.get(i)) {
+                buffer = (byte) (buffer | 1);
+            }
+
+            if (bufferPos == 8) {
+                lista.add(buffer);
+                buffer = 0;
+                bufferPos = 0;
+            }
+            i++;
+        }
+
+        if ((bufferPos < 8) && (bufferPos != 0)) {
+            buffer = (byte) (buffer << (8 - bufferPos));
+            lista.add(buffer);
+        }
+
+
+        byte[] ret = new byte[lista.size()];
+        for (int j = 0; j < ret.length; j++) {
+            ret[j] = lista.get(j);
+        }
+
+        return ret;
+    }
+
+    public static void guardarEnArchivo(String path, List<Boolean> mensajeCodificado){
+        try {
+            byte[] byteArray = convertBooleanListToByteArray(mensajeCodificado);
+            int tamanioEntero = (int) Math.ceil(mensajeCodificado.size() / 8d);
+            byte[] tamanio = new byte[4];
+            tamanio[0] = (byte) (tamanioEntero >> 24);
+            tamanio[1] = (byte) ((tamanioEntero << 8) >> 24);
+            tamanio[2] = (byte) ((tamanioEntero << 16) >> 24);
+            tamanio[3] = (byte) ((tamanioEntero << 24) >> 24);
+
+            FileOutputStream fos = new FileOutputStream(path);
+            fos.write(tamanio);
+            fos.write(byteArray);
+            fos.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static List<Boolean> codificarConHuffman(Imagen img){
+        List<Boolean> salida = new LinkedList<>();
+        Map<Integer, boolean[]> codificaciones = codificacionHuffman(img.probabilidadesSimples()).codificaciones;
 
         for (int y = 0; y < img.getHeight(); y++) {
             for (int x = 0; x < img.getWidth(); x++) {
                 int color = img.getColor(x, y);
-                salida.append(codificaciones.get(color));
+                boolean[] codificacion = codificaciones.get(color);
+                for (int i = 0; i < codificacion.length; i++){
+                    salida.add(codificacion[i]);
+                }
             }
         }
 
-        return salida.toString();
+        return salida;
     }
 
 
@@ -85,22 +187,32 @@ public class Codificaciones {
         }
     }
 
-    private static void armarCodificacion(Map<Integer, String> codificaciones, NodoHuffman nodo, String codificacion){
+    private static void armarCodificacion(Map<Integer, boolean[]> codificaciones, NodoHuffman nodo, List<Boolean> codificacion){
         if (nodo.simbolo != null){
-            codificaciones.put(nodo.simbolo, codificacion);
+            boolean[] cod = new boolean[codificacion.size()];
+            int i = 0;
+            for (Boolean b: codificacion){
+                cod[i] = b;
+                i++;
+            }
+            codificaciones.put(nodo.simbolo, cod);
         }else{
-            armarCodificacion(codificaciones, nodo.h1, codificacion + "0");
-            armarCodificacion(codificaciones, nodo.h2, codificacion + "1");
+            codificacion.add(false);
+            armarCodificacion(codificaciones, nodo.h1, codificacion);
+            codificacion.remove(codificacion.size() - 1);
+            codificacion.add(true);
+            armarCodificacion(codificaciones, nodo.h2, codificacion);
+            codificacion.remove(codificacion.size() - 1);
         }
     }
 
     private static class ResultadoCodificacion {
-        Map<Integer, String> codificaciones;
+        Map<Integer, boolean[]> codificaciones;
         NodoHuffman raiz;
     }
 
     public static ResultadoCodificacion codificacionHuffman(double frecuencias[]){
-        Map<Integer, String> codificacion = new HashMap<>();
+        Map<Integer, boolean[]> codificacion = new HashMap<>();
         PriorityQueue<NodoHuffman> colaDeNodosPadres = new PriorityQueue<>();
 
         //Agregar símbolos existentes con sus frecuencias
@@ -118,7 +230,7 @@ public class Codificaciones {
         }
 
         NodoHuffman raiz = colaDeNodosPadres.peek();
-        armarCodificacion(codificacion, raiz, "");
+        armarCodificacion(codificacion, raiz, new ArrayList<Boolean>());
 
         ResultadoCodificacion resultado = new ResultadoCodificacion();
         resultado.codificaciones = codificacion; resultado.raiz = raiz;
